674ca34133ec63ee6520ab74b46d6757a40320df
Peh Yew Hock <yewhock@PYH.local>
2018-10-10 18:05:15 +0800

    yew hock

--- brikis98/exercise-01/main.tf
+++ brikis98/exercise-01/main.tf
@@ -1,15 +1,23 @@
 # Look for a plugin called "Terraform" or "HCL" in your text editor / IDE to get syntax highlighting
 
 provider "aws" {
-  region = "eu-west-1" # You might want to use ap-southeast-1
+  region = "ap-southeast-1" # You might want to use ap-southeast-1
 }
 
 resource "aws_instance" "example" {
   # This is Ubuntu 18.04
   # You will have a different ID in ap-southeast-1
-  ami = "ami-00035f41c82244dab"
+  # ami = "ami-0fca02518e0faeb84"
+  ami = ${data.aws_ami.ubuntu.id}
 
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["${aws_security_group.example.id}"]
+
+  user_data = <<EOF
+  #!/bin/bash
+  echo "Hello, World" > index.html
+  nohup busybox httpd -f -p ${var.instance_http_port} &
+  EOF
 
   tags {
     # You'll want to change this to your own name
@@ -17,9 +25,58 @@ resource "aws_instance" "example" {
   }
 }
 
+resource "aws_security_group" "example" {
+  name = "${var.name}"
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = "${var.instance_http_port}"
+    to_port = "${var.instance_http_port}"
+    protocol = "tcp"
+    # Don't do this in production. Limit IPs in prod to trusted servers.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "instance_http_port" {
+  description = "The port the EC2 Instance will listen on for HTTP requests"
+  default = 8080
+}
+
 variable "name" {
   description = "Used to namespace all the resources"
-  default = "jim-testing-foo"
+  default = "pyh-var-test"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "image-type"
+    values = ["machine"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
 }
 
 output "public_ip" {
