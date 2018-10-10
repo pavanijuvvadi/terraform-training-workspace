9865116532efaa94ff3f9aa9fef07cbf980c8818
jxNets <Jingxia@nets.com.sg>
2018-10-10 18:19:33 +0800

    exercise 01 from jx

--- jxNets/exercise-01/main.tf
+++ jxNets/exercise-01/main.tf
@@ -13,7 +13,6 @@ resource "aws_instance" "example" {
     vpc_security_group_ids = ["${aws_security_group.example.id}"]
 
     user_data =<<EOF
-
 #!/bin/bash
 echo "Hello, World" > index.html
 nohup busybox httpd -f -p ${var.instance_http_port} &
