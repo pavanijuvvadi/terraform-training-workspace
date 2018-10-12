25c7b9d8a5defbe7ed13dfc459b026f949d88198
Yevgeniy Brikman <brikis98@gmail.com>
2018-10-12 10:02:25 +0100

    Make small tweak to user data

--- brikis98/exercise-01/main.tf
+++ brikis98/exercise-01/main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "example" {
 
   user_data = <<EOF
 #!/bin/bash
-echo "Hello, World" > index.html
+echo "Hello, World from $(hostname)" > index.html
 nohup busybox httpd -f -p ${var.instance_http_port} &
 EOF
 
