d0dafe948292d5a6cdda1deafde62f85941ef9f6
admin <admin@localhost.localdomain>
2018-10-15 19:57:11 +0800

    nelson updated exercise-04 and added exercise-05

---
+++ nelson/exercise-05/modules/dependencies.tf
@@ -0,0 +1,41 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners = ["099720109477"] # Canonical
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "image-type"
+    values = ["machine"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+}
+
+data "template_file" "user_data" {
+  template = "${file("${path.module}/user-data.sh")}"
+
+  vars {
+    name = "${var.name}"
+    port = "${var.instance_http_port}"
+  }
+}
