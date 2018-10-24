252b614c9b1e8ef7ee7794118b1d97cceecae6ab
jxNets <Jingxia@nets.com.sg>
2018-10-24 15:13:50 +0800

    add terraform module for user creation

--- jxNets/createUser/module/main.tf
+++ jxNets/createUser/module/main.tf
@@ -5,7 +5,7 @@ data "aws_caller_identity" "this" {
 resource "aws_iam_account_alias" "this" {
   account_alias = "${var.account_alias}"
 }
-
+//
 resource "aws_iam_account_password_policy" "this" {
   count = "${var.create_account_password_policy ? 1 : 0}"
 
